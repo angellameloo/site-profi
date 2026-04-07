57033b3aeabb85174f18ad2d08b544e0d004578e
angellameloo <angella.9833968@aluno.mg.gov.br>
2026-04-07 13:33:36 +0000

    modi4

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
+// ALERTA DE BOAS-VINDAS
 function boasVindas() {
-    alert("Bem-vindo ao site sobre Profissões do Futuro!");
+    alert("Bem-vindo ao site sobre Profissões do Futuro");
 }
 
-// Mudar texto
+// MUDAR TEXTO
 function mudarTexto() {
-    document.getElementById("texto").innerHTML = "Profissão: Engenheiro de Inteligência Artificial 🤖";
+    document.getElementById("texto").innerHTML =
+        "Profissão: Cientista de Dados ";
 }
 
-// Trocar cor
-function mudarCor() {
-    document.body.style.backgroundColor = "#c8e6c9";
+// CALCULADORA
+function somar() {
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+
+    let resultado = n1 + n2;
+
+    document.getElementById("resultado").innerHTML =
+        "Resultado: " + resultado;
 }
 
-// Mostrar / esconder
-function toggleTexto() {
-    let texto = document.getElementById("extra");
+// QUIZ
+function resposta(opcao) {
+    let resposta = document.getElementById("quizResposta");
 
-    if (texto.style.display === "none") {
-        texto.style.display = "block";
+    if (opcao === "b") {
+        resposta.innerHTML = "Correto! Engenheiro de IA é uma profissão do futuro.";
     } else {
-        texto.style.display = "none";
+        resposta.innerHTML = "Errado! Tente novamente.";
     }
 }
 
-// Contador
+// CONTADOR
 let numero = 0;
 
 function contar() {
